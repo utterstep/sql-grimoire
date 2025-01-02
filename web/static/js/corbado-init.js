b7e1ee63f06c8ddbf5b9505cee039f5438c35805
Vlad Stepanov <8uk.8ak@gmail.com>
2025-01-02 12:35:35 +0400

    enable Corbado debug

--- web/static/js/corbado-init.js
+++ web/static/js/corbado-init.js
@@ -2,6 +2,7 @@ if (!window.CORBADO_LOADED) {
     await Corbado.load({
         darkMode: 'on',
         projectId: 'pro-1480739848524663487',
+        debug: true,
     });
 
     window.CORBADO_LOADED = true;
